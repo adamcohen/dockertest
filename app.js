a3aaaba35da560b31fdab31f6092cadf0ca36141
Peter McInerney <pmcinerney@westfieldrs.com>
2018-09-27 20:44:11 -0700

    Add Skaffold

--- app.js
+++ app.js
@@ -3,5 +3,6 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/status', (req, res) => res.send('OK'))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
