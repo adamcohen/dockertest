38e6a3c879bf74cbe0a97a4d91f713033571e700
Adam Cohen <acohen@au.westfield.com>
2018-09-28 13:21:53 +1000

    [master] add sha output to /

--- app.js
+++ app.js
@@ -2,7 +2,12 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => {
+  res.json({
+    text: 'Hello World!',
+    sha: process.env.CURRENT_SHA || 'SHA not available'
+  })
+})
 
 app.get('/status', (req, res) => {
   res.status(200).send('ok');
